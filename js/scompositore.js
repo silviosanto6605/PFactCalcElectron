45d763d267818b6b2b681ac0427bd2772ce817f1
Silvio Santoriello <silviosanto6605@gmail.com>
2020-03-18 22:55:44 +0100

    beautified code, gui improvements

--- js/scompositore.js
+++ js/scompositore.js
@@ -1,26 +1,23 @@
-function scomposizione(){
+function scomposizione() {
 
     var N = document.getElementById("number").value;
-    if (isNaN(N) || (N<0)){
+    if (isNaN(N) || (N < 0)) {
         document.getElementById("result").innerHTML = "Inserisci un numero valido :(";
 
-    }
-
-    else{
-        var fattore = N.toString()+"=1"
+    } else {
+        var fattore = N.toString() + "=1"
         var d = 2;
 
-        while (N>=d){
-            if ((N%d)==0){
-                
-                fattore = fattore.toString()+"*"+d.toString();
-                N = N/d;
+        while (N >= d) {
+            if ((N % d) == 0) {
+
+                fattore = fattore.toString() + "*" + d.toString();
+                N = N / d;
 
 
-            }
-            else{
+            } else {
 
-                d = d+1;
+                d = d + 1;
 
 
             }
